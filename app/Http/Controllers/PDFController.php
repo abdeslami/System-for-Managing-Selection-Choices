d5d2e2eb96fb1367b203b3fa169323aa6a1c8594
ABDESLAMI <abdeslami.achraf@gmail.com>
2024-03-25 11:37:37 +0000

    jeiejiee

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -6,16 +6,20 @@ namespace App\Http\Controllers;
 use App\Models\Candidature;
 use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade\Pdf;
-
+use Illuminate\Support\Facades\Auth;
 
 class PDFController extends Controller
 {
     //
     public function generatePdf()
     {
-        $candidature = Candidature::find(1);
+        $userId=Auth::user()->id;
+        $candidature = Candidature::where('user_id',$userId)->first();
         $pdf = PDF::loadView('etudiant.fiche', compact('candidature'));
+        if ($candidature) {
+            
         return $pdf->stream('inscription.pdf', array('Content-Disposition' => 'inline'));
+        }
     }
 
 
